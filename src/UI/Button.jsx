cb347049381994c75db6147be569ee4681fa327d
blazejpak <blazej.pak@gmail.com>
2023-07-16 18:36:31 +0200

    done

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ onClick, text }) => {
   return (
     <div onClick={onClick} className="button">
       <div className="button_arrow">
-        <img src={downArrows} alt="Down Arrow" />
+        <img src={downArrows} alt="Down Arrow" width={16} height={14} />
       </div>
       <div className="button_text">
         <p className="button_text--style">{text}</p>
